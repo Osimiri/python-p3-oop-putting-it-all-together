2ecfe036f0f23b153b861c2caa9bd4b6418cf879
Justin Elue <mjuelue@gmail.com>
2023-02-27 12:58:40 -0600

    right order

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size = 0):
+        self.brand = brand 
+        self.size = size
+    
+    # def get_color(self):
+    #     return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_size(self):
+        return self._size
+
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size 
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)  #getter setter the deleter 
+
+    def get_material(self):
+        return self._material
+    
+    def get_condition(self):
+        return self._condition
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    
+
+
+
+  
